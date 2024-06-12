5c8e7f06315cbf71b307334430fdb15bbfb7be6c
MVignesh <vignesh.mariappan@pragmaticplay.com>
2024-06-12 21:41:53 +0530

    Tabs are modified and audio files are shifted to the new tabs code changes

--- src/assets/audios.js
+++ src/assets/audios.js
@@ -1,9 +1,11 @@
 /* Meditation images */ 
 import WellBeingImg from "./images/well-being.jpeg";
-import NoBigDealImg from "./images/no-big-deal.jpeg";
-import NoFakeNeedsImg from "./images/no-fake-needs.jpeg";
-import AvoidLimitingHabitsImg from "./images/avoid-limiting-habits.jpeg";
-import SeverelyBlessedImg from "./images/severely-blessed.jpeg";
+import DeHypeImg from "./images/de-hype.jpeg";
+import FakeNeedsReleaseImg from "./images/fake-needs-release.jpeg";
+import LimitingViewsAndHabitsImg from "./images/limiting-views-habits.jpeg";
+import BlessedConsciousnessImg from "./images/blessed-consciousness.jpeg";
+import BlessedConsciousnessMaxImg from "./images/blessed-consciousness-max.jpeg";
+import FinalMeditationImg from "./images/final-meditation.jpeg";
 
 /* Well being first */
 import WellBeingFirstOne from "./audios/Well_being_first/1-Ready_to_lose_getting_and_doing_but_not_being.mp3";
@@ -12,33 +14,39 @@ import WellBeingFirstThree from "./audios/Well_being_first/3-Success_due_to_well
 import WellBeingFirstFour from "./audios/Well_being_first/4-Well_being_gives_me_top_performance.mp3";
 import WellBeingFirstFive from "./audios/Well_being_first/5-Balance_bhava_tough_conversations_made_easy.mp3";
 
-/* No Fake Needs */
+/* Fake Need Release */
 import NoFakeNeedsOne from "./audios/Fake_needs/1-I_will_release_myself_from_jail.mp3";
 import NoFakeNeedsTwo from "./audios/Fake_needs/2-All_expectations_are_extras.mp3";
 import NoFakeNeedsThree from "./audios/Fake_needs/3-I_reject_all_marketing_and_being_particular.mp3";
-import NoFakeNeedsFour from "./audios/Fake_needs/4-I_will_not_universalize_like_a_baby_elephant.mp3";
-import NoFakeNeedsFive from "./audios/Fake_needs/5-I_will_playfully_break_2mm_everyday.mp3";
 
-/* No Big Deal */
+
+/* Dehype/De-Link */
 import NoBigDealOne from "./audios/No_big_deal/1-Black_hole_shrinking_no_big_deal.mp3";
 import NoBigDealTwo from "./audios/No_big_deal/2-I_accept_lifes_plan_for_me.mp3";
 import NoBigDealThree from "./audios/No_big_deal/3-I_will_be_a_gentle_judge_and_accept_them_for_who_they_are.mp3";
 import NoBigDealFour from "./audios/No_big_deal/4-I_release_the_dead_persons_weight.mp3";
 import NoBigDealFive from "./audios/No_big_deal/5-I_am_not_a_fool_I_was_just_asleep_in_my_situations.mp3";
 
-/* Avoid Limiting Habits */
-import AvoidLimitingHabitsOne from "./audios/Remove_limiting_habits_and_beliefs/1-True-story-is-not-exciting-and-i-will-not-let-the-river-dry.mp3";
-import AvoidLimitingHabitsTwo from "./audios/Remove_limiting_habits_and_beliefs/2-I_will_make_true_story_choice_like_a_child.mp3";
-import AvoidLimitingHabitsThree from "./audios/Remove_limiting_habits_and_beliefs/3-I_acknowledge_the_ocean_of_mercy.mp3";
-import AvoidLimitingHabitsFour from "./audios/Remove_limiting_habits_and_beliefs/4-I_am_significantly_thankful.mp3";
-import AvoidLimitingHabitsFive from "./audios/Remove_limiting_habits_and_beliefs/5-Life_will_work_out_and_I_will_focus_on_ocean_of_blessings.mp3";
+/* Limiting Views & Habits */
+import LimitingViewsAndHabitsOne from "./audios/Remove_limiting_habits_and_beliefs/1-I_will_not_universalize_like_a_baby_elephant.mp3";
+import LimitingViewsAndHabitsTwo from "./audios/Remove_limiting_habits_and_beliefs/2-I_will_playfully_break_2mm_everyday.mp3";
+import LimitingViewsAndHabitsThree from "./audios/Remove_limiting_habits_and_beliefs/3-True-story-is-not-exciting-and-i-will-not-let-the-river-dry.mp3";
+import LimitingViewsAndHabitsFour from "./audios/Remove_limiting_habits_and_beliefs/4-I_will_make_true_story_choice_like_a_child.mp3";
 
-/* Severely Blessed */
-import SeverelyBlessedOne from "./audios/Severely_blessed/1-My_setbacks_are_my_blessings.mp3";
-import SeverelyBlessedTwo from "./audios/Severely_blessed/2-There_is_no_wave_there_is_only_ocean.mp3";
-import SeverelyBlessedThree from "./audios/Severely_blessed/3-I_will_treat_everything_like_a_bonus.mp3";
-import SeverelyBlessedFour from "./audios/Severely_blessed/4-I_learn_during_bad_times_and_I_progress_during_good_times.mp3";
 
+/* Blessed Consciousness */
+import BlessedConsciousnessOne from "./audios/Blessed_consciousness/1-I_acknowledge_the_ocean_of_mercy.mp3";
+import BlessedConsciousnessTwo from "./audios/Blessed_consciousness/2-I_am_significantly_thankful.mp3";
+import BlessedConsciousnessThree from "./audios/Blessed_consciousness/3-Life_will_work_out_and_I_will_focus_on_ocean_of_blessings.mp3";
+
+/* Blessed Consciousness Max */
+import BlessedConsciousnessMaxOne from "./audios/Blessed_consciousness_max/1-My_setbacks_are_my_blessings.mp3";
+import BlessedConsciousnessMaxTwo from "./audios/Blessed_consciousness_max/2-There_is_no_wave_there_is_only_ocean.mp3";
+import BlessedConsciousnessMaxThree from "./audios/Blessed_consciousness_max/3-I_will_treat_everything_like_a_bonus.mp3";
+import BlessedConsciousnessMaxFour from "./audios/Blessed_consciousness_max/4-I_learn_during_bad_times_and_I_progress_during_good_times.mp3";
+
+/* Final Meditation */
+import FinalMeditationOne from "./audios/Final_meditation/1-Final_meditation.mp3";
 
 export const AUDIOS = {
     "Well Being First": [
@@ -77,147 +85,155 @@ export const AUDIOS = {
             imgSrc: WellBeingImg
         },
     ],
-    "No Fake Needs": [
+    "Fake Need Release": [
         {
-            id: "NFN01",
+            id: "FNR01",
             audioTitle: "I'll release myself from jail",
             audioPath: NoFakeNeedsOne,
-            imgSrc: NoFakeNeedsImg
+            imgSrc: FakeNeedsReleaseImg
         },
 
         {
-            id: "NFN02",
+            id: "FNR02",
             audioTitle: "All expectations are extras",
             audioPath: NoFakeNeedsTwo,
-            imgSrc: NoFakeNeedsImg
+            imgSrc: FakeNeedsReleaseImg
         },
 
         {
-            id: "NFN03",
+            id: "FNR03",
             audioTitle: "I reject all marketing and being particular",
             audioPath: NoFakeNeedsThree,
-            imgSrc: NoFakeNeedsImg
+            imgSrc: FakeNeedsReleaseImg
         },
 
-        {
-            id: "NFN04",
-            audioTitle: "I will not universalize like a baby elephant",
-            audioPath: NoFakeNeedsFour,
-            imgSrc: NoFakeNeedsImg
-        },
-
-        {
-            id: "NFN05",
-            audioTitle: "I'll playfully break 2mm everyday",
-            audioPath: NoFakeNeedsFive,
-            imgSrc: NoFakeNeedsImg
-        },
+        
     ],
-    "No Big Deal": [
+    "Dehype/De-Link": [
         {
-            id: "NBD01",
+            id: "DHDL01",
             audioTitle: "Black hole shrinking no big deal",
             audioPath: NoBigDealOne,
-            imgSrc: NoBigDealImg
+            imgSrc: DeHypeImg
         },
 
         {
-            id: "NBD02",
+            id: "DHDL02",
             audioTitle: "I accept life's plan for me",
             audioPath: NoBigDealTwo,
-            imgSrc: NoBigDealImg
+            imgSrc: DeHypeImg
         },
 
         {
-            id: "NBD03",
+            id: "DHDL03",
             audioTitle: "I'll be the gentle judge and accept them for who they are",
             audioPath: NoBigDealThree,
-            imgSrc: NoBigDealImg
+            imgSrc: DeHypeImg
         },
 
         {
-            id: "NBD04",
+            id: "DHDL04",
             audioTitle: "I release the dead person's weight",
             audioPath: NoBigDealFour,
-            imgSrc: NoBigDealImg
+            imgSrc: DeHypeImg
         },
 
         {
-            id: "NBD05",
+            id: "DHDL05",
             audioTitle: "I'm not a fool, I was asleep in my situations",
             audioPath: NoBigDealFive,
-            imgSrc: NoBigDealImg
+            imgSrc: DeHypeImg
         },
     ],
 
-    "Avoid Limiting Habits": [
+    "Limiting Views & Habits": [
         {
-            id: "AVH01",
-            audioTitle: "True story is not exciting and I won't let the river dry",
-            audioPath: AvoidLimitingHabitsOne,
-            imgSrc: AvoidLimitingHabitsImg
+            id: "LVH01",
+            audioTitle: "I will not universalize like a baby elephant",
+            audioPath: LimitingViewsAndHabitsOne,
+            imgSrc: LimitingViewsAndHabitsImg
         },
 
         {
-            id: "AVH02",
-            audioTitle: "I'll make true story choice like a child",
-            audioPath: AvoidLimitingHabitsTwo,
-            imgSrc: AvoidLimitingHabitsImg
+            id: "LVH02",
+            audioTitle: "I'll playfully break 2mm everyday",
+            audioPath: LimitingViewsAndHabitsTwo,
+            imgSrc: LimitingViewsAndHabitsImg
         },
 
         {
             id: "AVH03",
-            audioTitle: "I acknowledge the ocean of mercy",
-            audioPath: AvoidLimitingHabitsThree,
-            imgSrc: AvoidLimitingHabitsImg
+            audioTitle: "True story is not exciting and I won't let the river dry",
+            audioPath: LimitingViewsAndHabitsThree,
+            imgSrc: LimitingViewsAndHabitsImg
         },
 
         {
             id: "AVH04",
-            audioTitle: "I'm significantly thankful",
-            audioPath: AvoidLimitingHabitsFour,
-            imgSrc: AvoidLimitingHabitsImg
-        },
-
-        {
-            id: "AVH05",
-            audioTitle: "Life will work out and I'll focus on ocean of blessings",
-            audioPath: AvoidLimitingHabitsFive,
-            imgSrc: AvoidLimitingHabitsImg
+            audioTitle: "I'll make true story choice like a child",
+            audioPath: LimitingViewsAndHabitsFour,
+            imgSrc: LimitingViewsAndHabitsImg
         },
     ],
-    "Severely Blessed": [
+    "Blessed Consciousness": [
         {
-            id: "SB01",
+            id: "BC01",
+            audioTitle: "I acknowledge the ocean of mercy",
+            // audioTitle: "My Setbacks are my blessings",
+            audioPath: BlessedConsciousnessOne,
+            imgSrc: BlessedConsciousnessImg
+        },
+
+        {
+            id: "BC02",
+            audioTitle: "I'm significantly thankful",
+            // audioTitle: "There is no wave, there is only ocean",
+            audioPath: BlessedConsciousnessTwo,
+            imgSrc: BlessedConsciousnessImg
+        },
+
+        {
+            id: "BC03",
+            audioTitle: "Life will work out and I will focus on the ocean of blessings",
+            audioPath: BlessedConsciousnessThree,
+            imgSrc: BlessedConsciousnessImg
+        },
+    ],
+    "Blessed Consciousness Max": [
+        {
+            id: "BCM01",
             audioTitle: "My Setbacks are my blessings",
-            audioPath: SeverelyBlessedOne,
-            imgSrc: SeverelyBlessedImg
+            audioPath: BlessedConsciousnessMaxOne,
+            imgSrc: BlessedConsciousnessMaxImg
         },
 
         {
-            id: "SB02",
+            id: "BCM02",
             audioTitle: "There is no wave, there is only ocean",
-            audioPath: SeverelyBlessedTwo,
-            imgSrc: SeverelyBlessedImg
+            audioPath: BlessedConsciousnessMaxTwo,
+            imgSrc: BlessedConsciousnessMaxImg
         },
 
         {
-            id: "SB03",
+            id: "BCM03",
             audioTitle: "I will treat everything like a bonus",
-            audioPath: SeverelyBlessedThree,
-            imgSrc: SeverelyBlessedImg
+            audioPath: BlessedConsciousnessMaxThree,
+            imgSrc: BlessedConsciousnessMaxImg
         },
 
         {
-            id: "SB04",
+            id: "BCM04",
             audioTitle: "I learn during bad times and I progress during good times",
-            audioPath: SeverelyBlessedFour,
-            imgSrc: SeverelyBlessedImg
+            audioPath: BlessedConsciousnessMaxFour,
+            imgSrc: BlessedConsciousnessMaxImg
         },
-
-        // {
-        //     id: "SB05",
-        //     audioPath: "./audios/Severely_blessed/1-My_setbacks_are_my_blessings.mp3"
-        // },
+    ],
+    "Final Meditation": [
+        {
+            id: "FM01",
+            audioTitle: "Final Meditation",
+            audioPath: FinalMeditationOne,
+            imgSrc: FinalMeditationImg
+        },
     ]
 }
--- src/components/PickYourMeditation.jsx
+++ src/components/PickYourMeditation.jsx
@@ -6,7 +6,7 @@ import 'animate.css';
 
 const PickYourMeditation = () => {
   const [ selectedMeditation, setSelectedMeditation ] = useState(1);
-  const meditationTypeClasses = `p-4 text-center font-semibold max-[375px]:text-[10px] text-[13px] md:font-bold text-white rounded-md cursor-pointer`;
+  const meditationTypeClasses = `p-4 text-center font-semibold max-[335px]:text-[9px] max-[385px]:text-[11px] text-[13px] md:font-bold text-white rounded-md cursor-pointer`;
   const outlineClasses = `outline outline-primary outline-3 outline-offset-4`;
   const [ selectedMeditationAudios, setSelectedMeditationAudios ] = useState(AUDIOS["Well Being First"]);
   const [ currentPlayingAudioId, setCurrentPlayingAudioId ] = useState(null);
@@ -17,21 +17,29 @@ const PickYourMeditation = () => {
         setSelectedMeditation(1);
         setSelectedMeditationAudios(AUDIOS["Well Being First"]);
         break;
-      case "No Big Deal":
+      case "Dehype/De-Link":
         setSelectedMeditation(2);
-        setSelectedMeditationAudios(AUDIOS["No Big Deal"]);
+        setSelectedMeditationAudios(AUDIOS["Dehype/De-Link"]);
         break;
-      case "No Fake Needs":
+      case "Fake Need Release":
         setSelectedMeditation(3);
-        setSelectedMeditationAudios(AUDIOS["No Fake Needs"]);
+        setSelectedMeditationAudios(AUDIOS["Fake Need Release"]);
         break;
-      case "Avoid Limiting Habits":
+      case "Limiting Views & Habits":
         setSelectedMeditation(4);
-        setSelectedMeditationAudios(AUDIOS["Avoid Limiting Habits"]);
+        setSelectedMeditationAudios(AUDIOS["Limiting Views & Habits"]);
         break;
-      case "Severely Blessed":
+      case "Blessed Consciousness":
         setSelectedMeditation(5);
-        setSelectedMeditationAudios(AUDIOS["Severely Blessed"]);
+        setSelectedMeditationAudios(AUDIOS["Blessed Consciousness"]);
+        break;
+      case "Blessed Consciousness Max":
+        setSelectedMeditation(6);
+        setSelectedMeditationAudios(AUDIOS["Blessed Consciousness Max"]);
+        break;
+      case "Final Meditation":
+        setSelectedMeditation(7);
+        setSelectedMeditationAudios(AUDIOS["Final Meditation"]);
         break;
       default:
         setSelectedMeditation(1);
@@ -43,12 +51,14 @@ const PickYourMeditation = () => {
   return (
     <div className="w-full flex flex-col gap-2.5 mb-8">
         {/* <h2 className="text-3xl font-normal text-[#eab308] my-4">Pick a meditation</h2> */}
-        <div className="grid grid-cols-2 lg:grid-cols-5 justify-center gap-3 items-stretch sm:min-w-[200px] text-nowrap animate__animated animate__fadeIn animate__slow" onClick={meditationTypeClickHandler}>
+        <div className="grid grid-cols-2 lg:grid-cols-4 justify-center gap-3 items-stretch sm:min-w-[200px] text-nowrap animate__animated animate__fadeIn animate__slow" onClick={meditationTypeClickHandler}>
           <div className={`${meditationTypeClasses} ${ selectedMeditation === 1 && outlineClasses } bg-[#e11d48]`}>Well Being First</div>
-          <div className={`${meditationTypeClasses} ${ selectedMeditation === 2 && outlineClasses } bg-[#0369a1]`}>No Big Deal</div>
-          <div className={`${meditationTypeClasses} ${ selectedMeditation === 3 && outlineClasses } bg-[#2e1065]`}>No Fake Needs</div>
-          <div className={`${meditationTypeClasses} ${ selectedMeditation === 4 && outlineClasses } bg-[#4a044e]`}>Avoid Limiting Habits</div>
-          <div className={`${meditationTypeClasses} ${ selectedMeditation === 5 && outlineClasses } col-span-2 md:col-span-1 bg-[#ea580c]`} /* className="p-4 text-white font-black flex-1 outline outline-primary outline-3 outline-offset-4 text-[16px]  rounded-md bg-[#ea580c] cursor-pointer" */>Severely Blessed</div>
+          <div className={`${meditationTypeClasses} ${ selectedMeditation === 2 && outlineClasses } bg-[#0369a1]`}>Dehype/De-Link</div>
+          <div className={`${meditationTypeClasses} ${ selectedMeditation === 3 && outlineClasses } bg-[#2e1065]`}>Fake Need Release</div>
+          <div className={`${meditationTypeClasses} ${ selectedMeditation === 4 && outlineClasses } bg-[#4a044e]`}>Limiting Views & Habits</div>
+          <div className={`${meditationTypeClasses} ${ selectedMeditation === 5 && outlineClasses } col-span-2 md:col-span-1 bg-[#ea580c]`}>Blessed Consciousness</div>
+          <div className={`${meditationTypeClasses} ${ selectedMeditation === 6 && outlineClasses } col-span-2 md:col-span-1 bg-[#7e22ce]`}>Blessed Consciousness Max</div>
+          <div className={`${meditationTypeClasses} ${ selectedMeditation === 7 && outlineClasses } col-span-2 md:col-span-1 bg-[#16a34a]`}>Final Meditation</div>
         </div>
 
         <div className="flex flex-col items-center gap-5 my-4">
