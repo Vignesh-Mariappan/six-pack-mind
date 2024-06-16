11b452aa98e2e48f4895d4725176b2fbdfd76387
MVignesh <vignesh.mariappan@pragmaticplay.com>
2024-06-16 21:11:45 +0530

    removed base url from vite config and homepage from package json files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Vignesh-Mariappan.github.io/six-pack-mind/',
+  // base: 'https://Vignesh-Mariappan.github.io/six-pack-mind/',
 });
+
+/* 
+ add the following in package.json for hosting in github pages
+ "homepage": "https://Vignesh-Mariappan.github.io/six-pack-mind",
+*/
