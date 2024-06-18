91844c1f06ce9f02863267c2a46900375a2ba027
MVignesh <vignesh.mariappan@pragmaticplay.com>
2024-06-18 13:12:07 +0530

    dvh is added in css for mobile landscape

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -3,9 +3,9 @@ import SixPackMindIcon from "../assets/logo/six-pack-mind-icon-transparent.png";
 
 const LoadingPage = ({ progressWidth }) => {
   return (
-    <div className="flex w-[100vw] h-[100vh] overflow-hidden flex-col items-center justify-center gap-3">
+    <div className="flex w-[100vw] h-[100dvh] overflow-hidden flex-col items-center justify-center gap-3">
         <div></div>
-        <div className="bg-no-repeat bg-contain bg-center w-[200px] h-[200px]" style={{ backgroundImage: `url(${SixPackMindIcon})` }}></div>
+        <div className="bg-no-repeat bg-contain bg-center w-[150px] h-[150px]" style={{ backgroundImage: `url(${SixPackMindIcon})` }}></div>
         <progress className="progress progress-primary w-56" value={progressWidth} max="100"></progress>
     </div>
   )
